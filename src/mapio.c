e1a8ada9c0a557bb7f3a16ff9cae193e9e904215
Lucas Ghibert <lghibert@emi.u-bordeaux1.fr>
2016-11-25 14:26:08 +0100

    Màj mapio.c

--- src/mapio.c
+++ src/mapio.c
@@ -9,8 +9,7 @@
 
 #ifdef PADAWAN
 
-void map_new (unsigned width, unsigned height)
-{
+void map_new (unsigned width, unsigned height){
   map_allocate (width, height);
 
   for (int x = 0; x < width; x++)
@@ -37,25 +36,21 @@ void map_new (unsigned width, unsigned height)
   map_object_add ("images/coin.png", 20, MAP_OBJECT_AIR | MAP_OBJECT_COLLECTIBLE);
 
   map_object_end ();
-
 }
 
-void map_save (char *filename)
-{
-  // TODO
+void map_save (char *filename){
 	int mapSave = open(filename, O_WRONLY|O_CREAT, 0666);
 
-	//char t = '\t';
 	char n = '\n';
+	int endOfMap = -1;
 	int width = map_width();
 	int height = map_height();
 	int nbObjects = map_objects();
 
-	int object, nameObj, framesObj, solidityObj, destructibleObj, collectibleObj, generatorObj;
+	int object, nameObj;//, framesObj, solidityObj, destructibleObj, collectibleObj, generatorObj;
 
 	write(mapSave, &width, sizeof(int));
 	write(mapSave, &height, sizeof(int));
-	//write(mapSave, &t, sizeof(char));
 	write(mapSave, &nbObjects, sizeof(int));
 	write(mapSave, &n, sizeof(char));
 
@@ -75,40 +70,32 @@ void map_save (char *filename)
 					nameObj = 4;
 				else if(!strcmp(map_get_name(object), "images/coin.png"))
 					nameObj = 5;
-				framesObj = map_get_frames(object);
+				/*framesObj = map_get_frames(object);
 				solidityObj = map_get_solidity(object);
 				destructibleObj = map_is_destructible(object);
 				collectibleObj = map_is_collectible(object);
-				generatorObj = map_is_generator(object);
+				generatorObj = map_is_generator(object);*/
 
 				write(mapSave, &i, sizeof(int));
 				write(mapSave, &j, sizeof(int));
 				write(mapSave, &nameObj, sizeof(int));
-				//write(mapSave, &t, sizeof(char));
-				write(mapSave, &framesObj, sizeof(int));
-				//write(mapSave, &t, sizeof(char));
+				/*write(mapSave, &framesObj, sizeof(int));
 				write(mapSave, &solidityObj, sizeof(int));
-				//write(mapSave, &t, sizeof(char));
 				write(mapSave, &destructibleObj, sizeof(int));
-				//write(mapSave, &t, sizeof(char));
 				write(mapSave, &collectibleObj, sizeof(int));
-				//write(mapSave, &t, sizeof(char));
-				write(mapSave, &generatorObj, sizeof(int));
+				write(mapSave, &generatorObj, sizeof(int));*/
 				write(mapSave, &n, sizeof(char));				
 			}
 		}
+	write(mapSave, &endOfMap, sizeof(int));
+	//write(mapSave, ,);
 
 	printf("Map save performed to the end\n");
-	//fprintf (stderr, "Sorry: Map save is not yet implemented\n");
 }
 
-void map_load (char *filename)
-{
-  // TODO
+void map_load (char *filename){
 	int mapLoad = open(filename, O_RDONLY, 0666);
-	/*while(read(mapLoad, &d, sizeof(int))!=0){
-		
-	}*/
+	
 	char n;
 	int width=0;
 	int height=0;
@@ -122,40 +109,28 @@ void map_load (char *filename)
 		map_allocate (width, height);
 
 	// Chargement map
-	int x, y, nameObj, framesObj, solidityObj, destructibleObj, collectibleObj, generatorObj;
-	while(read(mapLoad, &x, sizeof(int))){ 
+	int x, y, nameObj;
+	while(read(mapLoad, &x, sizeof(int))!=-1){
 		read(mapLoad, &y, sizeof(int));
 		read(mapLoad, &nameObj, sizeof(int));
-		read(mapLoad, &framesObj, sizeof(int));
-		read(mapLoad, &solidityObj, sizeof(int));
-		read(mapLoad, &destructibleObj, sizeof(int));
-		read(mapLoad, &collectibleObj, sizeof(int));
-		read(mapLoad, &generatorObj, sizeof(int));
 		read(mapLoad, &n, sizeof(char));
 		map_set(x, y, nameObj);
 	}
 
-
-
 	if(nbObjects!=0)
 		map_object_begin (nbObjects);
 
-	// Texture pour le sol
-	map_object_add ("images/ground.png", 1, MAP_OBJECT_SOLID);
-	// Mur
-	map_object_add ("images/wall.png", 1, MAP_OBJECT_SOLID);
-	// Gazon
-	map_object_add ("images/grass.png", 1, MAP_OBJECT_SEMI_SOLID);
-	// Marbre
-	map_object_add ("images/marble.png", 1, MAP_OBJECT_SOLID | MAP_OBJECT_DESTRUCTIBLE);
-	// Fleurs
-	map_object_add ("images/flower.png", 1, MAP_OBJECT_AIR);
-	// Pièce
-	map_object_add ("images/coin.png", 20, MAP_OBJECT_AIR | MAP_OBJECT_COLLECTIBLE);
+	for(int i=0 ; i<nbObjects ; i++){
+		read(mapLoad, ,);
+		/*map_object_add ("images/ground.png", 1, MAP_OBJECT_SOLID);
+		map_object_add ("images/wall.png", 1, MAP_OBJECT_SOLID);
+		map_object_add ("images/grass.png", 1, MAP_OBJECT_SEMI_SOLID);
+		map_object_add ("images/marble.png", 1, MAP_OBJECT_SOLID | MAP_OBJECT_DESTRUCTIBLE);
+		map_object_add ("images/flower.png", 1, MAP_OBJECT_AIR);
+		map_object_add ("images/coin.png", 20, MAP_OBJECT_AIR | MAP_OBJECT_COLLECTIBLE);*/
+	}
 
 	map_object_end ();
-
-	//exit_with_error ("Map load is not yet implemented\n");
 }
 
 #endif
