afb59247ce26b8e1c31d3db0ebafddcc7f79a920
Lucas Ghibert <lghibert@emi.u-bordeaux1.fr>
2016-11-28 18:23:58 +0100

    maputil.c --getwidth, --getheight, --getobjects, --getinfo

--- util/maputil.c
+++ util/maputil.c
@@ -7,23 +7,32 @@
 void usage(char * commande){
 	fprintf(stderr, "%s fichier commande (entier/listeCaracteristiquesObjets)\n", commande);
 	exit(EXIT_FAILURE);
-} 
+}
 
 int main(int argc, char ** argv){
 	if(argc<3)
 		usage(argv[0]);
 
-	int mapFile = 0;
-	//char * commande = malloc(strlen(argv[2])*sizeof(char));
+	int mapFile, c;
 	char * commande = argv[2];
-	if(!strcmp(commande, "--getwidth") || !strcmp(commande, "--getheigth") || !strcmp(commande, "--getobjects") || !strcmp(commande, "--getinfo")){
+	if(!strcmp(commande, "--getwidth") || !strcmp(commande, "--getheight") || !strcmp(commande, "--getobjects") || !strcmp(commande, "--getinfo")){
 		mapFile = open(argv[1], O_RDONLY, 0666);
 		if(mapFile==-1){
 			fprintf(stderr, "Le fichier ne s'est pas ouvert correctement.\n");
 			exit(EXIT_FAILURE);
+		}else{
+			int k = 1;
+			if(!strcmp(commande, "--getheight"))
+				lseek(mapFile, sizeof(int), SEEK_SET);
+			if(!strcmp(commande, "--getobjects"))
+				lseek(mapFile, 2*sizeof(int), SEEK_SET);
+			if(!strcmp(commande, "--getinfo"))
+				k=3;
+			for(int i=0 ; i<k ; i++){
+				read(mapFile, &c, sizeof(int));
+				printf("%d\n", c);
+			}
 		}
-
-
 	}else if(!strcmp(commande, "--setwidth"), !strcmp(commande, "--setheight"), !strcmp(commande, "--setobjects"), !strcmp(commande, "--pruneobjects")){
 		mapFile = open(argv[1], O_WRONLY, 0666);
 		if(mapFile==-1){
@@ -36,6 +45,5 @@ int main(int argc, char ** argv){
 		fprintf(stderr, "Commande invalide.\n");
 		exit(EXIT_FAILURE);
 	}
-	//free(commande);
 	return EXIT_SUCCESS;
 }                                                                                                                                                                      
