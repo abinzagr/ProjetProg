4c64d6c4966f606cb98d1025e75259be03adfcd5
Lucas Ghibert <lghibert@emi.u-bordeaux1.fr>
2016-11-29 13:38:29 +0100

    maputil.c début --setwidth --setheight

--- util/maputil.c
+++ util/maputil.c
@@ -12,35 +12,78 @@ void usage(char * commande){
 int main(int argc, char ** argv){
 	if(argc<3)
 		usage(argv[0]);
-
-	int mapFile, c;
+	int mapFile, c, i = -1;
 	char * commande = argv[2];
-	if(!strcmp(commande, "--getwidth") || !strcmp(commande, "--getheight") || !strcmp(commande, "--getobjects") || !strcmp(commande, "--getinfo")){
+
+	if(!strcmp(commande, "--getwidth"))
+		i=0;
+	if(!strcmp(commande, "--getheight"))
+		i=1;
+	if(!strcmp(commande, "--getobjects"))
+		i=2;
+	if(!strcmp(commande, "--getinfo"))
+		i=3;
+	if(!strcmp(commande, "--setwidth"))
+		i=4;
+	if(!strcmp(commande, "--setheight"))
+		i=5;
+	if(!strcmp(commande, "--setobjects"))
+		i=6;
+	if(!strcmp(commande, "--pruneobjects"))
+		i=7;
+
+	if(i>=0 && i<=3){
 		mapFile = open(argv[1], O_RDONLY, 0666);
 		if(mapFile==-1){
 			fprintf(stderr, "Le fichier ne s'est pas ouvert correctement.\n");
 			exit(EXIT_FAILURE);
-		}else{
-			int k = 1;
-			if(!strcmp(commande, "--getheight"))
-				lseek(mapFile, sizeof(int), SEEK_SET);
-			if(!strcmp(commande, "--getobjects"))
-				lseek(mapFile, 2*sizeof(int), SEEK_SET);
-			if(!strcmp(commande, "--getinfo"))
-				k=3;
-			for(int i=0 ; i<k ; i++){
-				read(mapFile, &c, sizeof(int));
-				printf("%d\n", c);
-			}
 		}
-	}else if(!strcmp(commande, "--setwidth"), !strcmp(commande, "--setheight"), !strcmp(commande, "--setobjects"), !strcmp(commande, "--pruneobjects")){
-		mapFile = open(argv[1], O_WRONLY, 0666);
+		int k = 1;
+		if(i==1)
+			lseek(mapFile, sizeof(int), SEEK_SET);
+		if(i==2)
+			lseek(mapFile, 2*sizeof(int), SEEK_SET);
+		if(i==3)
+			k=3;
+		for(int j=0 ; j<k ; j++){
+			read(mapFile, &c, sizeof(int));
+			printf("%d\n", c);
+		}
+	}else if(i>=4){ //&& i<=7){
+		mapFile = open(argv[1], O_RDWR, 0666); // ouvrir fichier original en RD_ONLY, créer copy modifier en WR_ONLY.
 		if(mapFile==-1){
 			fprintf(stderr, "Le fichier ne s'est pas ouvert correctement.\n");
 			exit(EXIT_FAILURE);
 		}
-
-
+		if(i!=7){
+			if(argc<4){
+				fprintf(stderr, "Quatrieme argument requis.\n");
+				usage(argv[0]);
+			}
+			if(i==4 || i==5){
+				int arg = atoi(argv[3]);
+				int wh, x = 0, y, n;
+				if(arg==0){
+					fprintf(stderr, "Quatrieme argument invalide (requiert un entier).\n");
+					exit(EXIT_FAILURE);
+				}
+				if(i==5)
+					lseek(mapFile, sizeof(int), SEEK_SET);
+				read(mapFile, &wh, sizeof(int));
+				lseek(mapFile, -sizeof(int), SEEK_CUR);
+				write(mapFile, &arg, sizeof(int));
+				lseek(mapFile, 3*sizeof(int), SEEK_SET);
+				while(x!=-1){
+					read(mapFile, &x, sizeof(int));
+					if(x!=-1){
+						read(mapFile, &y, sizeof(int));
+						read(mapFile, &n, sizeof(int));
+					}
+				} // A FINIR
+			}else{
+			}
+		}else{
+		}
 	}else{
 		fprintf(stderr, "Commande invalide.\n");
 		exit(EXIT_FAILURE);
