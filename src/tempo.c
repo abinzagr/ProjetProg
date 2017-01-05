b8ba7ce9d3d66a9dfffdb4215f2ae08bfbb7ad2f
Laetitia Leoni <laeleoni@emi.u-bordeaux1.fr>
2017-01-05 19:51:49 +0100

    maj handler, ne marche pas lignes 43 a 66

--- src/tempo.c
+++ src/tempo.c
@@ -36,28 +36,54 @@ echeancier tab_echeancier[200];
 
 /*handler demon*/
 void hand(int s)
-{	printf("\nSignal %d recu\n", s);
-	//printf ("sdl_push_event(%p) appelée au temps %ld\n", param, get_time ());
-	sdl_push_event(param);
-	//recuperer le premier indice ou expire =false
-	//sdl_push_event
-	//
+{
+  printf("\nSignal %d recu\n", s);
+  //printf ("sdl_push_event(%p) appelée au temps %ld\n", param, get_time ());
+  
+  /*unsigned long t = get_time();
+  bool all_event_finished = false;
+  struct itimerval t_event = tab_echeancier[0].timer;
+  int i_event = 0;
+  while(all_event_finished == false){
+    all_event_finished = true;
+    //on recupere l'indice de l'evenement le plus recent et qui n'a pas encore ete effectue
+    for(int i = 0; i < 200; i++){
+      if(tab_echeancier[i].timer.it_value.tv_sec < t_event.it_value.tv_sec && tab_echeancier[i].expire == false){
+	t_event = tab_echeancier[i].timer;
+	i_event = i;
+      }
+    }
+    //on traite l'evenement le plus recent
+    sdl_push_event(tab_echeancier[i].param);
+    tab_echeancier[i_event].expire = true;
+    //si tous les evenements n'ont pas ete effectues, on permet a la boucle while
+    //de continuer
+    for(int i = 0; i < 200; i++){
+      if(tab_echeancier[i].expire == false)
+	all_event_finished = false;
+    }
+  }
+  */
+  sdl_push_event(param); //  a mettre avant
+  //recuperer le premier indice ou expire =false
+  //sdl_push_event
+  //
 }
 
 /*Demon fonction threads */
 void *Demon(void *p)
-{		printf("%p: bonjour ! ", pthread_self()); 
-		struct sigaction action;
-		action.sa_handler = hand;
-		sigset_t my_set;
-		sigfillset(&my_set);
-		sigdelset(&my_set,SIGALRM);
-		sigprocmask(SIG_BLOCK, &my_set,NULL);
-		sigaction (SIGALRM, &action, NULL);
-		while(1){
-			sigsuspend(&(my_set));
-		}
-		
+{
+  printf("%p: bonjour ! ", pthread_self()); 
+  struct sigaction action;
+  action.sa_handler = hand;
+  sigset_t my_set;
+  sigfillset(&my_set);
+  sigdelset(&my_set,SIGALRM);
+  sigprocmask(SIG_BLOCK, &my_set,NULL);
+  sigaction (SIGALRM, &action, NULL);
+  while(1){
+    sigsuspend(&(my_set));
+  }		
 }
 
 
@@ -84,6 +110,7 @@ int timer_init (void)
     tab_echeancier[i].timer.it_value.tv_sec = 0;
     tab_echeancier[i].timer.it_value.tv_usec = 0;
     //init expire ? param ?
+    tab_echeancier[i].expire = true;
   }
  
   return 1; 
@@ -107,6 +134,7 @@ void timer_set (Uint32 delay, void *parametre)
   tab_echeancier[i].timer.it_value.tv_sec=sec;
   tab_echeancier[i].timer.it_value.tv_usec=usec;
   tab_echeancier[i].param = parametre;
+  tab_echeancier[i].expire = false;
   setitimer(ITIMER_REAL, &(tab_echeancier[i].timer), NULL); 
   	
   //while (1);
